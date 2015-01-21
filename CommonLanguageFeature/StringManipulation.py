a746a4599e1c7ff793110d979bce877637e5a23b
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-01-22 00:00:26 +0530

    Added some string manipulation codes

--- CommonLanguageFeature/StringManipulation.py
+++ CommonLanguageFeature/StringManipulation.py
@@ -12,3 +12,42 @@ pi = 3.14
 
 print (str(pi))
 
+myvairablestring = "Hello World \t Jalpesh"
+
+print(myvairablestring)
+
+myvairablestring =" Hello world \n Jalpesh"
+
+print(myvairablestring)
+
+#print escape sequence with raise
+
+print(r"Testng \n back slash")
+
+#print multiple license
+print("""
+
+	Hello world Jalpesh 
+
+	This is a multiple line print
+ """
+)
+
+# using -1 as string manipulation
+
+#-1 will give last character
+
+name ="Jalpesh"
+
+print (name[-1])
+
+# character range
+print(name[0:2])
+
+print (name[:2])
+
+#string concentatination 
+
+print("Jalpesh" + "Vadgama")
+
+print("K" + name[1:])
