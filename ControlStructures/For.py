393230e475c243d89dda8338886dcaf1851ff466
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-01-22 00:22:08 +0530

    Added For structure files

---
+++ ControlStructures/For.py
@@ -0,0 +1,21 @@
+words = ['Hello','World', "Jalpesh"]
+
+for w in words:
+	print (w, len(w))
+
+# range function
+
+for i in range(5):
+	print(i)
+
+#range function variation
+
+for i in range(5,10):
+	print(i)
+
+for i in range(5,15,2):
+	print(i)
+
+for i in range(len(words)):
+	print(i, words[i])
+
