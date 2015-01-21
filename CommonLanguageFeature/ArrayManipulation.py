5cb8d217eac15e4668086c04027a6e39fc40e7cd
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-01-22 00:06:39 +0530

    Added Array manipulation file

---
+++ CommonLanguageFeature/ArrayManipulation.py
@@ -0,0 +1,16 @@
+square = [1,4,9]
+
+## printing array
+print (square[:])
+
+## adding elements to array
+square = square + [16,25]
+
+## printing again after add
+
+print(square[:])
+
+##updating specific item
+
+square[4] =36
+print(square[:])
